9fe8638456e6b6206549afad1388f8591aa127b1
Paul Leber <luaprebel@gmail.com>
2025-01-06 07:39:33 -0800

    Add automatic id creation to new Book instances

--- booktracker/main.py
+++ booktracker/main.py
@@ -1,6 +1,6 @@
 import argparse
 import csv
-from dataclasses import dataclass
+from dataclasses import asdict, dataclass, field
 from datetime import date
 from enum import StrEnum
 from pathlib import Path
@@ -16,25 +16,44 @@ class Status(StrEnum):
 
 @dataclass
 class Book:
-    title: str
-    author: str
+    title: str = ""
+    author: str = ""
     status: Status = "TBR"
     date_started: date | None = None
     date_completed: date | None = None
+    id: int = field(init=False)
+
+    def __post_init__(self):
+        books = read_books()
+        if books:
+            last_book_id = int(books[-1]["id"])
+            self.id = last_book_id + 1
+        else:
+            self.id = 1
+
+
+def read_books(path: Path = Path("booktracker/book_db.csv")) -> list[str]:
+    if path.is_file():
+        with open(path) as f:
+            reader = csv.DictReader(f)
+            return [row for row in reader]
+    else:
+        return []
 
 
 def write_book(book: Book) -> None:
     path = Path("booktracker/book_db.csv")
+    book_dict = asdict(book)
     if path.is_file():
         with open(path, "a", newline="\n") as f:
             writer = csv.writer(f)
-            writer.writerow(book.__dict__.values())
+            writer.writerow(book_dict.values())
     else:
         with open(path, "w", newline="\n") as f:
             fieldnames = list(Book.__annotations__)
             writer = csv.DictWriter(f, fieldnames=fieldnames)
             writer.writeheader()
-            writer.writerow(book.__dict__)
+            writer.writerow(book_dict)
     print(f"Added {book.title} by {book.author} to book_db.csv")
 
 
